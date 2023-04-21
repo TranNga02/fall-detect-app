069f4ff9256c7b59800d447b8feccd25faa8453c
TranNga02 <tranngadn02@gmail.com>
2023-04-21 16:32:27 +0700

    done notification

--- app/src/main/java/com/example/fall_detect/view/activity/MainActivity.java
+++ app/src/main/java/com/example/fall_detect/view/activity/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.fall_detect.R;
 import com.example.fall_detect.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FCMToken"; // Định nghĩa TAG là "FCMToken"
     private ActivityMainBinding binding;
 
     @Override
@@ -22,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
         NavigationUI.setupWithNavController(binding.navBottom, navController);
+
+        getFcmToken();
+    }
+
+    public void getFcmToken() {
+
+        // Lấy FCM token của thiết bị
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        String fcmToken = task.getResult();
+                        // Lưu trữ FCM token lên Firebase Realtime Database hoặc Firestore
+                        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid(); // Lấy ID của người dùng hiện tại
+                        FirebaseDatabase.getInstance().getReference("/users/" + currentUserId + "/deviceTokens")
+                                .setValue(fcmToken)
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        Log.d(TAG, "Lưu trữ FCM token thành công");
+                                    } else {
+                                        Log.e(TAG, "Lưu trữ FCM token thất bại", task1.getException());
+                                    }
+                                });
+                    } else {
+                        Log.e(TAG, "Lấy FCM token thất bại", task.getException());
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/fall_detect/viewmodel/MyFirebaseMessagingService.java
@@ -0,0 +1,80 @@
+package com.example.fall_detect.viewmodel;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.fall_detect.R;
+import com.example.fall_detect.view.fragment.WelcomeFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+
+        System.out.println("--------------------------------------------------------");
+
+        if(message.getNotification() != null) {
+            showNotification(message.getNotification().getTitle(), message.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        System.out.println("-----------------------------"+token);
+        String userId = FirebaseAuth.getInstance().getUid();
+        // Write a message to the database
+        FirebaseDatabase.getInstance().getReference("/users/" + userId + "/deviceTokens")
+                .setValue(token);
+    }
+
+    private void showNotification(String title, String messageBody) {
+
+        Intent intent = new Intent(this, WelcomeFragment.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_IMMUTABLE);
+
+        String channelId = "fcm_default_channel";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_notifications)
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
